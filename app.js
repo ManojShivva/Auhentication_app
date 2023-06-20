12a9e41781b4e5ff34de5bab34e6789279c96091
ManojShivva <shivvamanojkumar8888@gmail.com>
2023-06-20 23:19:03 +0530

    3rd commit with complete passport OAuth

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const mongoose = require("mongoose");
 const session = require("express-session"); //cookies:1
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -28,20 +30,80 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose); //cookies:4
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy()); //for local login strategy //cookies:5
-passport.serializeUser(User.serializeUser()); //for creating a cookie with user details
-passport.deserializeUser(User.deserializeUser()); //for destroying a cookie to fetch user details
+
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id).then(function(user){
+    done(null,user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function(req,res){
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
+
+app.get('/auth/facebook',passport.authenticate('facebook', { scope: ['user_friends'] }));
+
+  app.get('/auth/facebook/secret',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/secrets');
+    });
 
 app.get("/login", function(req,res){
   res.render("login");
@@ -52,16 +114,41 @@ app.get("/register", function(req,res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({secret: {$exists:true}}).then(function(foundData){
+    if(foundData){
+      console.log(foundData);
+      res.render("secrets", {userWithSecrets: foundData});
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
 });
 
-app.get("/logout", function(req, res){
-  req.logout();
-  res.redirect("/");
+app.post("/submit", function(req, res){
+  const subSec = req.body.secret;
+
+  User.findById(req.user.id).then(function(foundUser){
+    if(foundUser){
+      foundUser.secret = subSec;
+      foundUser.save();
+      res.redirect("/secrets");
+    }
+  });
+});
+
+app.get("/logout", function(req, res, next){
+  req.logout(function(err){
+    if(err){
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 app.post("/register", function(req, res){
